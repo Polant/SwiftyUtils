a98949b483175691339b69855e044fc86b5b640a
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2017-08-16 21:26:46 +0300

    Initial commit

---
+++ SwiftyUtils/SwiftyUtils.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyUtils.h
+//  SwiftyUtils
+//
+//  Created by Anton Poltoratskyi on 16.08.17.
+//  Copyright © 2017 Anton Poltoratskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyUtils.
+FOUNDATION_EXPORT double SwiftyUtilsVersionNumber;
+
+//! Project version string for SwiftyUtils.
+FOUNDATION_EXPORT const unsigned char SwiftyUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyUtils/PublicHeader.h>
+
+
